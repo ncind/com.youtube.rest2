53504d3a6f1c8624713a1294ec62c39660010c6c
Nikhil Pagidala <Nikhil_Pagidala@intuit.com>
2015-04-18 13:06:37 -0700

    send conn

---
+++ src/com/youtube/dao/OracleVMDB.java
@@ -0,0 +1,33 @@
+package com.youtube.dao;
+
+import javax.naming.*;
+import javax.sql.*;
+
+public class OracleVMDB {
+
+	private static DataSource OracleVM = null;
+	private static Context context = null;
+
+	public static DataSource OracleVMConn() throws Exception {
+
+		if(OracleVM != null){
+			return OracleVM;
+		}
+		
+		try {
+			
+			if(context == null){
+				context = new InitialContext();
+			}
+			
+			OracleVM = (DataSource)context.lookup("VMOracle");
+
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+
+		return OracleVM;
+	}
+
+}
--- src/com/youtube/rest2/status/V1_status.java
+++ src/com/youtube/rest2/status/V1_status.java
@@ -2,6 +2,8 @@ package com.youtube.rest2.status;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
+import java.sql.*;
+import com.youtube.dao.*;
 
 
 /**
@@ -55,5 +57,46 @@ public class V1_status {
 	}
 	
 	
+	@Path("/database")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnDatabaseStatus() throws Exception {
+		PreparedStatement query = null;
+		String myString = null;
+		String returnString = null;
+		Connection conn = null;
+		
+		try {
+			// Get Database Connection and establish connection
+			conn = OracleVMDB.OracleVMConn().getConnection();
+			
+			// Write the SQL Query and have java pre-compile it. (Improves Speed and safety)
+			query = conn.prepareStatement("select to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS') DATETIME " + "from sys.dual");
+			ResultSet rs = query.executeQuery();
+			
+			// Send pre-compiled SQL to Oracle for data.
+			
+			while(rs.next()){
+				// /*Debug*/ System.out.println(rs.getString("DATETIME"));
+				
+				myString = rs.getString("DATETIME");
+								
+			}
+			
+			query.close(); // Close Connection
+			
+			returnString = "<p>Database Status </p>" + "<p>Database Date/Time return: " + myString + "</p>";
+			
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally{
+			
+			if(conn != null){conn.close();}
+		}
+		return returnString;
+	}
 	
 }
