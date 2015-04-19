95fac7c3476c2854bf8c30f7da2c64c7dfac64b7
Nikhil Pagidala <Nikhil_Pagidala@intuit.com>
2015-04-19 15:24:46 -0700

    k

--- src/com/youtube/dao/SchemaVMDB.java
+++ src/com/youtube/dao/SchemaVMDB.java
@@ -48,5 +48,45 @@ public class SchemaVMDB extends OracleVMDB{
 	}
 	
 	
+public JSONArray queryReturnBrandItemNumber(String brand, int item_number) throws Exception{
+		
+		PreparedStatement query = null;
+		Connection conn = null;
+		
+		ToJSON converter = new ToJSON();
+		JSONArray json = new JSONArray();
+			
+		try {
+			conn = oraclePcPartsConnection();
+			
+			query = conn.prepareStatement("select PC_PARTS_PK, PC_PARTS_TITLE, PC_PARTS_CODE, PC_PARTS_MAKER, PC_PARTS_AVAIL, PC_PARTS_DESC " +
+								 "from PC_PARTS " +
+								 "where UPPER(PC_PARTS_MAKER) = ? " +
+								 "and PC_PARTS_CODE = ? "
+					);
+			query.setString(1, brand.toUpperCase());
+			query.setInt(2, item_number);		
+			ResultSet rs = query.executeQuery();
+			
+			json = converter.toJSONArray(rs);
+			query.close(); // close connection
+			
+			
+		} catch (SQLException sqlError) {
+			sqlError.printStackTrace();
+			return json;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return json;
+		}
+		finally{
+			if(conn != null) conn.close();
+		}
+		
+		
+		return json;
+		
+	}
+	
 
 }
--- src/com/youtube/rest2/inventory/V2_inventory.java
+++ src/com/youtube/rest2/inventory/V2_inventory.java
@@ -1,5 +1,6 @@
 package com.youtube.rest2.inventory;
 
+import javax.print.attribute.standard.Media;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -9,6 +10,7 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.QueryParam;
 
 import org.codehaus.jettison.json.JSONArray;
+
 import com.youtube.dao.SchemaVMDB;
 
 @Path("/v2/inventory")
@@ -39,5 +41,78 @@ public class V2_inventory {
 		return Response.ok(returnString).build();
 		
 	}
+	
+	
+	// Method if the above default GET is not present
+	/*@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response returnErrorOnBrand() throws Exception{
+		return Response.status(400).entity("Error : Please specify brand for this search").build();
+	}*/
+	
+	@Path("/{brand}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response returnBrand(
+				@PathParam("brand") String brand)
+							throws Exception{
+		
+		String returnString = null;
+		JSONArray json = new JSONArray();
+		
+		try {
+			SchemaVMDB dao = new SchemaVMDB();
+			
+			json = dao.queryReturnBrandParts(brand);
+			returnString = json.toString();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(500).entity("Server was not able to process your request").build();
+		}
+		
+		return Response.ok(returnString).build();
+		
+	}
+	
+	
+	@Path("/{brand}/{item_number}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response returnSpecificBrandItem(@PathParam("brand") String brand,
+											@PathParam("item_number") int item_number) 
+											throws Exception{
+		
+		String returnString = null;
+		JSONArray json = new JSONArray();
+		
+		try {
+			SchemaVMDB dao = new SchemaVMDB();
+			
+			json = dao.queryReturnBrandItemNumber(brand, item_number);
+			returnString = json.toString();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(500).entity("Server was not able to process your request").build();
+		}
+		
+		return Response.ok(returnString).build();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
