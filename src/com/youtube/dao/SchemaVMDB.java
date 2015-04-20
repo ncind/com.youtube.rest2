3f8d2c024d6d435fa8ad077061e43d41a9dc0ed8
Nikhil Pagidala <Nikhil_Pagidala@intuit.com>
2015-04-20 01:43:18 -0700

    commit

--- src/com/youtube/dao/SchemaVMDB.java
+++ src/com/youtube/dao/SchemaVMDB.java
@@ -9,46 +9,43 @@ import com.youtube.utilities.*;
 public class SchemaVMDB extends OracleVMDB {
 
 	// Insert SQL Statment
-	public int insertIntoPC_Parts(String PC_PARTS_TITLE,  String PC_PARTS_CODE, String PC_PARTS_MAKER, String PC_PARTS_AVAIL, String PC_PARTS_DESC) throws Exception{
-		
-		
+	public int insertIntoPC_Parts(String PC_PARTS_TITLE, String PC_PARTS_CODE,
+			String PC_PARTS_MAKER, String PC_PARTS_AVAIL, String PC_PARTS_DESC)
+			throws Exception {
+
 		PreparedStatement query = null;
 		Connection conn = null;
-		
+
 		try {
-			
+
 			conn = oraclePcPartsConnection();
-			query = conn.prepareStatement("insert into PC_PARTS " +
-					"(PC_PARTS_TITLE, PC_PARTS_CODE, PC_PARTS_MAKER, PC_PARTS_AVAIL, PC_PARTS_DESC) " +
-					"VALUES ( ?, ?, ?, ?, ? ) ");
-			
+			query = conn
+					.prepareStatement("insert into PC_PARTS "
+							+ "(PC_PARTS_TITLE, PC_PARTS_CODE, PC_PARTS_MAKER, PC_PARTS_AVAIL, PC_PARTS_DESC) "
+							+ "VALUES ( ?, ?, ?, ?, ? ) ");
+
 			query.setString(1, PC_PARTS_TITLE);
 			query.setString(2, PC_PARTS_CODE);
 			query.setString(3, PC_PARTS_MAKER);
-			
+
 			int avilInt = Integer.parseInt(PC_PARTS_AVAIL);
 			query.setInt(4, avilInt);
-			
+
 			query.setString(5, PC_PARTS_DESC);
 			query.executeUpdate();
-			
-			
-			
-			
+
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
 			return 500;
+		} finally {
+			if (conn != null)
+				conn.close();
 		}
-		finally{
-			if (conn != null) conn.close();
-		}
-		
+
 		return 200;
 	}
-	
-	
-	
+
 	public JSONArray queryReturnBrandParts(String brand) throws Exception {
 
 		PreparedStatement query = null;
@@ -124,4 +121,62 @@ public class SchemaVMDB extends OracleVMDB {
 
 	}
 
+	public int updatePC_PARTS(int pk, int avail) throws Exception {
+
+		PreparedStatement query = null;
+		Connection conn = null;
+
+		try {
+			/*
+			 * If this was a real application, you should do data validation
+			 * here before updating data.
+			 */
+
+			conn = oraclePcPartsConnection();
+			query = conn.prepareStatement("update PC_PARTS "
+					+ "set PC_PARTS_AVAIL = ? " + "where PC_PARTS_PK = ? ");
+
+			query.setInt(1, avail);
+			query.setInt(2, pk);
+			query.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 500;
+		} finally {
+			if (conn != null)
+				conn.close();
+		}
+
+		return 200;
+	}
+
+	public int deletePC_PARTS(int pk) throws Exception {
+
+		PreparedStatement query = null;
+		Connection conn = null;
+
+		try {
+			/*
+			 * If this was a real application, you should do data validation
+			 * here before deleting data.
+			 */
+
+			conn = oraclePcPartsConnection();
+			query = conn.prepareStatement("delete from PC_PARTS "
+					+ "where PC_PARTS_PK = ? ");
+
+			query.setInt(1, pk);
+			query.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 500;
+		} finally {
+			if (conn != null)
+				conn.close();
+		}
+
+		return 200;
+	}
 }
--- src/com/youtube/rest2/inventory/V3_inventory.java
+++ src/com/youtube/rest2/inventory/V3_inventory.java
@@ -1,8 +1,10 @@
 package com.youtube.rest2.inventory;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -64,5 +66,110 @@ public class V3_inventory {
 		
 		
 	}
+	
+	
+	@Path("/{brand}/{item_number}")
+	@PUT
+	@Consumes({MediaType.APPLICATION_FORM_URLENCODED, MediaType.APPLICATION_JSON})
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response updateItem(@PathParam("brand") String brand, 
+								@PathParam("item_number") int item_number,
+								String  incomingData) throws Exception{
+		
+		int pk;
+		int avail;
+		int http_code;
+		
+		String returnString = null;
+		JSONArray jsonArray = new JSONArray();
+		JSONObject jsonObject = new JSONObject();
+		SchemaVMDB dao = new SchemaVMDB();
+		
+		try {
+			
+			JSONObject partsData = new JSONObject(incomingData);
+			pk = partsData.optInt("PC_PARTS_PK", 0);
+			avail = partsData.optInt("PC_PARTS_AVAIL", 0);
+			
+			//call the sql method
+			http_code =  dao.updatePC_PARTS(pk, avail);
+			
+			if(http_code == 200){
+				jsonObject.put("HTTP_CODE", "200");
+				jsonObject.put("MSG", "Item has been updated succesfully");
+			} else {
+				return Response.status(500).entity("Server was not able to process your request").build();
+			}
+			
+			returnString = jsonArray.put(jsonObject).toString();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			return Response.status(500).entity("Server was not able to process your request").build();
+		}
+		
+		return Response.ok(returnString).build();
+	}
+	
+	@Path("/{brand}/{item_number}")
+	@DELETE
+	@Consumes({MediaType.APPLICATION_FORM_URLENCODED,MediaType.APPLICATION_JSON})
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response deleteItem(@PathParam("brand") String brand,
+									@PathParam("item_number") int item_number,
+									String incomingData) 
+								throws Exception {
+		
+		//System.out.println("incomingData: " + incomingData);
+		//System.out.println("brand: " + brand);
+		//System.out.println("item_number: " + item_number);
+		
+		int pk;
+		int http_code;
+		String returnString = null;
+		JSONArray jsonArray = new JSONArray();
+		JSONObject jsonObject = new JSONObject();
+		SchemaVMDB dao = new SchemaVMDB();
+		
+		try {
+			
+			JSONObject partsData = new JSONObject(incomingData);
+			pk = partsData.optInt("PC_PARTS_PK", 0);
+			
+			http_code = dao.deletePC_PARTS(pk);
+			
+			if(http_code == 200) {
+				jsonObject.put("HTTP_CODE", "200");
+				jsonObject.put("MSG", "Item has been deleted successfully");
+			} else {
+				return Response.status(500).entity("Server was not able to process your request").build();
+			}
+			
+			returnString = jsonArray.put(jsonObject).toString();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			return Response.status(500).entity("Server was not able to process your request").build();
+		}
+		
+		return Response.ok(returnString).build();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
